1c8efea30116e814a7bb086365d783146fc1db13
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-09-08 22:07:08 -0300

    adicionando arquivos

--- src/stores/storeArtigo.js
+++ src/stores/storeArtigo.js
@@ -5,6 +5,10 @@ import tempoRefletir from "@/assets/imagens/tempoRefletir.jpg";
 import escolhas from "@/assets/imagens/escolhas.jpg";
 import amanhecer from "@/assets/imagens/amanhecer.jpg";
 import amiltonMenezes from "@/assets/imagens/amiltonMenezes.jpg";
+import coracaoQuebrantado from "@/assets/imagens/coracaoQuebrantado.jpg";
+import olhosdeDeus from "@/assets/imagens/olhosdeDeus.jpg";
+import ansiedade from "@/assets/imagens/ansiedade.jpg";
+import aflicoes from "@/assets/imagens/aflicoes.jpg";
 
 export const artigosStore = defineStore({
 	id: "artigos",
@@ -32,11 +36,33 @@ export const artigosStore = defineStore({
 				titulo: "O plano de Deus para você",
 			},
 			{
-				imagem: escolhas,
+				imagem: aflicoes,
 				data: "Set 05, 2023",
 				autorImagem: amiltonMenezes,
 				autor: "Amilton Menezes",
 				titulo: "Muitas são as aflições do justo, o Senhor de todas o livra",
+			},
+			{
+				imagem: coracaoQuebrantado,
+				data: "Set 06, 2023",
+				autorImagem: amiltonMenezes,
+				autor: "Amilton Menezes",
+				titulo:
+					" Opiniões conflitantes tornam-se campos de batalha entre indivíduos. ",
+			},
+			{
+				imagem: olhosdeDeus,
+				data: "Set 07, 2023",
+				autorImagem: amiltonMenezes,
+				autor: "Amilton Menezes",
+				titulo: "Como enxergar com os olhos de Deus?",
+			},
+			{
+				imagem: ansiedade,
+				data: "Set 08, 2023",
+				autorImagem: amiltonMenezes,
+				autor: "Amilton Menezes",
+				titulo: "Substitua a ansiedade",
 			},
 		],
 	}),
@@ -49,6 +75,16 @@ export const artigosStore = defineStore({
 	actions: {
 		criarNovoArtigo(artigo) {
 			this.artigos.push(artigo); // Adicione o novo artigo ao estado diretamente
+		},
+		async carregarArtigos() {
+			try {
+				// Fazer uma solicitação para carregar os artigos do JSON
+				const response = await fetch("@/data/artigos.json");
+				const data = await response.json();
+				this.artigos = data.artigos;
+			} catch (error) {
+				console.error("Erro ao carregar artigos:", error);
+			}
 		},
 	},
 });
