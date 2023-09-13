96e884d79d6e1513b0a40f39daa631cefad1d5ee
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-09-13 17:49:53 -0300

    adicionando novos artigos

--- src/stores/storeArtigo.js
+++ src/stores/storeArtigo.js
@@ -22,7 +22,7 @@ export const artigosStore = defineStore({
 				autor: "Amilton Menezes",
 				titulo: "O tempo nunca para !",
 				conteudo:
-					"O relógio da vida só para uma vez e homem algum tem poder	mesmo de imaginar, quando os ponteiros vão deixar de girar.				Cedo ou tarde? Impossível saber, agora é o único tempo que				tendes à mão. Não confieis no amanhã, pois o relógio poderá	estar parado então.",
+					"O relógio da vida só para uma vez e homem algum tem poder	mesmo de imaginar, quando os ponteiros vão deixar de girar.	Cedo ou tarde? Impossível saber, agora é o único tempo que tendes à mão. Não confieis no amanhã, pois o relógio poderá	estar parado então.",
 			},
 			{
 				categoria: "#autoajuda",
@@ -78,8 +78,38 @@ export const artigosStore = defineStore({
 				data: "Set 08, 2023",
 				autorImagem: amiltonMenezes,
 				autor: "Amilton Menezes",
+				titulo: "Uma questão do coração",
+				conteudo:
+					"Salmo 119:11 – Guardei no coração a Tua palavra para não pecar contra Ti. O Salmo 119 é um poema com 22 estrofes, e cada uma dessas estrofes leva como título uma das 28 letras do alfabeto hebraico. O título da primeira estrofe é Aleph, a primeira letra do alfabeto, e assim todas as estrofes são nomeadas com o restante das letras. No maior livro da Bíblia, o maior capítulo exalta e celebra a lei, a Palavra de Deus. A Bíblia é chamada pelo salmista como “Tua Palavra”. A Bíblia pode ser, na estante, o livro mais importante ou pode ser como qualquer outro livro. A diferença está na quantidade de tempo que lhe dedicamos e para que a usamos. Uma das coisas fascinantes na Bíblia é que ela é um livro para gente jovem. Verso 9 – Como pode o jovem manter pura a sua conduta? Nesse verso está a pergunta mais difícil a ser respondida por um jovem: Como pode se manter puro diante de tanta imoralidade aberta e disponível a qualquer hora? Já que tem tanta energia e hormônios à flor da pele, como o jovem pode ser e se manter puro? Como podemos salvar o coração, a mente e o corpo do jovem? Seria bom que essa pergunta fizesse parte de sua oração: “Como posso manter puro meu coração?” A resposta é clara. A Bíblia deve ser nosso guia e devemos cuidar para exercitar vigilância, para que nossa vida esteja de acordo com o que Deus pede. Verso 11 – Guardei no coração a Tua palavra. Que lugar melhor existiria ou que solo seria mais fértil para a Bíblia do que um novo coração? Vamos esconder, entesourar, guardar dentro do coração renovado a Palavra de Deus! Um famoso pregador resumia o verso 11 da seguinte maneira: “A melhor coisa, no melhor lugar, para o melhor propósito.” Ou seja, a melhor coisa – a Palavra de Deus; no melhor lugar – o coração; para o melhor propósito – guardar-nos de pecar. “A Tua palavra é lâmpada que ilumina os meus passos e luz que clareia o meu caminho” (Sl 119:105). A lei, ou a Palavra de Deus, não se apresenta como espelho, mas como lâmpada para nossos pés. E já que é uma lâmpada para os pés, somos capazes de andar confiantes onde antes tropeçávamos. “Uma luz, nova e preciosa, irradia de cada página. A verdade é revelada, palavras e frases se tornam claras e apropriadas para a ocasião” (Parábolas de Jesus, p. 132). A Bíblia é mesmo a Palavra de Deus! Vamos orar e pedir que Deus nos ajude a separar tempo para estudá-la? Querido Deus, muitas vezes corremos o dia todo atrás do urgente e deixamos de lado aquilo que é importante. Ajuda-nos a passarmos mais tempo contigo, com a Tua Palavra. Em nome de Jesus, amém!",
+			},
+			{
+				categoria: "#autoajuda",
+				imagem: ansiedade,
+				data: "Set 11, 2023",
+				autorImagem: amiltonMenezes,
+				autor: "Amilton Menezes",
 				titulo: "Substitua a ansiedade",
 				conteudo: "vazio",
+			},
+			{
+				categoria: "#autoajuda",
+				imagem: ansiedade,
+				data: "Set 12, 2023",
+				autorImagem: amiltonMenezes,
+				autor: "Amilton Menezes",
+				titulo: "Sua verdadeira identidade",
+				conteudo:
+					"Quando alguém pergunta “Quem é você?”, provavelmente sua primeira resposta será “Meu nome é…”. Se você der também o sobrenome, posso chegar mais perto ainda de sua identidade. Se eu for visitar uma universidade e fizer a mesma pergunta, ouvirei muitas respostas: “Sou novato”, “faço Administração”, “toco na banda”, “canto no coral”, etc.\n\nHá outro grupo que encontra sua identidade no trabalho e vai responder: sou médico, advogado, professor, empresário, tenho meu próprio negócio, etc. Outros, ainda, encontram sua identidade se referindo ao lugar de nascimento: sou nordestino, paulista, gaúcho, etc. Para alguns, você nem precisa perguntar “Quem é você?”, porque, quando vêm falar com você, têm um crachá com o nome e a função que exercem. Sua identidade fica resumida à identificação que têm naquele pedaço de plástico.\n\nDepois de tudo isso que você leu/ouviu, pergunto: O que torna você mais você? Você está contente com a pessoa que é? Que outras características pessoais você quer ter? Há alguma coisa sobre você que o faz diferente de todos os demais e o torna especial? Você está contente com a pessoa em que se tornou?\n\nHoje o mundo utiliza diferentes padrões para avaliar nossa identidade. Um deles está relacionado com posses. Você se sentiria uma pessoa mais importante se tivesse uma BMW ou um Uno Mille? Se em lugar de um Casio tivesse um Rolex? Depois de se preocupar com o que tem, sua reação pode ser igual à declaração de alguém que disse: “Levo minha vida comprando coisas que eu não preciso, com dinheiro que eu não tenho, para impressionar pessoas de quem eu não gosto.”\n\nQuando nossa identidade está em Cristo, já não vamos nos preocupar pelo fato de sermos medidos de acordo com os padrões do mundo. Quanto mais eu ensaiar quem sou em Cristo, mais perto vou chegar da identidade que Deus deseja que eu tenha.\n\nQuem somos nós? Sal da terra e luz do mundo (Mt 5:13, 14), e amigos de Cristo (Jo 15:15). Fomos redimidos e perdoados de todos os nossos pecados (Cl 1:14). Nada pode nos separar do amor de Deus (Rm 8:39). Podemos todas as coisas em Cristo, que nos fortalece (Fp 4:13).\n\nO melhor que Jesus pode dizer sobre a nova identidade é que somos uma nova criação: “Se alguém está em Cristo, é nova criação. As coisas antigas já passaram; eis que surgiram coisas novas” (2Co 5:17).\n\nReflita sobre isso no dia de hoje e ore comigo agora:\n\nQuerido Deus, obrigado pela escolha que fizestes em salvar a nossa vida do pecado e pelo privilégio da vida eterna. Somos teus filhos! Em nome de Jesus, amém!",
+			},
+			{
+				categoria: "#autoajuda",
+				imagem: ansiedade,
+				data: "Set 13, 2023",
+				autorImagem: amiltonMenezes,
+				autor: "Amilton Menezes",
+				titulo: "Confiança e felicidade",
+				conteudo:
+					"A vida é uma rosa cheia de espinhos. Há problemas, todos os dias. Diante deles, o ser humano tem apenas duas alternativas: ou confia em Deus e segue as instruções divinas ou confia no próprio entendimento e tenta encontrar a saída para seus problemas nas próprias forças.\n\nConfiar é condição necessária para desenvolver qualquer relacionamento. Não há como viver sem confiar. Tudo que fazemos envolve confiança. Confiamos no padeiro, no motorista de ônibus, no piloto do avião… Muitas vezes, a confiança é traída. Por mais que o ser humano seja bom e tente cumprir suas promessas, está limitado pela sua própria humanidade. Por exemplo: Eu prometo dar uma bicicleta para meu neto no final do ano, e o que acontece se eu morrer dentro de um mês?\n\nPromessas humanas são falíveis, por serem humanas. Intenções humanas, com frequência, são egoístas e mentirosas. Nascem de um coração contaminado pelo vírus do pecado. Projetos humanos são passageiros e limitados, por causa da temporalidade da criatura.\n\nPor isso, o conselho de Salomão é: “O que confia no Senhor, esse é feliz.” É loucura confiar em Deus e, ao mesmo tempo, procurar a solução para os problemas da vida no esforço humano. “Confiar no Senhor” significa entrega, submissão e obediência. Essas atitudes não são próprias da natureza humana, mas são a única garantia de vitória. Por isso, o texto diz: “Atenta para o ensino [e] acha o bem.”\n\nComo todo dia, hoje também é um dia de decisões. Você está vivo. Viver é decidir. Para o bem ou para o mal, para a tristeza ou para a alegria. Dê a Deus a chance de ser o seu guia. Afinal de contas, o Criador conhece o caminho melhor do que a criatura. Ele é Deus. Suas promessas nunca falham.",
 			},
 		],
 	}),
