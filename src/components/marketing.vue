d76517c04100a8be840d0778a1f9d8461e820ffb
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-10-04 23:06:31 -0300

    adicionando login , add bibli cookie

---
+++ src/components/marketing.vue
@@ -0,0 +1,31 @@
+<template>
+	<div
+		class="container-marketing"
+		:style="{ backgroundImage: 'url(' + imageBackground + ')' }">
+		<img :src="logo" alt="Logo Life com Deus" />
+		<div class="information">
+			<span>Deus</span>
+
+			<br />
+			<span>Vida</span>
+
+			como a sua.
+		</div>
+		<nav class="social"></nav>
+	</div>
+</template>
+
+<script>
+import imageBackground from "@/assets/imagens/amanhecer.jpg";
+
+export default {
+	name: "marketing",
+	components: {},
+	data() {
+		return {
+			imageBackground: imageBackground,
+			logo: logo,
+		};
+	},
+};
+</script>
---
+++ src/views/loginView.vue
@@ -0,0 +1,34 @@
+<template>
+	<main id="main">
+		<section id="marketing">
+			<Marketing />
+		</section>
+		<section id="login">
+			<Login />
+		</section>
+	</main>
+</template>
+<script>
+import Marketing from "@/components/marketing.vue";
+import Login from "@/components/login.vue";
+
+export default {
+	name: "Layout",
+	components: {
+		Marketing,
+		Login,
+	},
+};
+</script>
+
+<style lang="postcss" scoped>
+#main {
+	@apply flex min-h-screen;
+}
+#marketing {
+	@apply w-2/3 hidden xl:block;
+}
+#login {
+	@apply xl:w-1/3 w-full bg-base-100;
+}
+</style>
