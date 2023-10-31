4897688099453a6f91ce85fc8d8296caa855af3e
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-10-30 23:35:14 -0300

    ajuste mobile header

--- front-end/src/components/header.vue
+++ front-end/src/components/header.vue
@@ -92,7 +92,7 @@ export default {
 			<!-- Background backdrop, show/hide based on slide-over state. -->
 			<div class="fixed inset-0 z-10"></div>
 			<div
-				class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+				class="right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
 				<div class="flex items-center justify-between">
 					<a href="#" class="-m-1.5 p-1.5">
 						<span class="sr-only">Vida livre</span>
@@ -101,21 +101,6 @@ export default {
 							src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
 							alt="" />
 					</a>
-					<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-						<span class="sr-only">Fechar Menu</span>
-						<svg
-							class="h-6 w-6"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							aria-hidden="true">
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M6 18L18 6M6 6l12 12" />
-						</svg>
-					</button>
 				</div>
 				<div class="mt-6 flow-root">
 					<div class="-my-6 divide-y divide-gray-500/10">
