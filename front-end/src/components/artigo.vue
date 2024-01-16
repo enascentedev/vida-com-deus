8de203c2561a2f9e4761d332aeb31a7a32002f15
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-01-15 21:59:00 -0300

    refatorando o componente artigo

--- front-end/src/components/artigo.vue
+++ front-end/src/components/artigo.vue
@@ -1,3 +1,35 @@
+<template>
+	<div id="artigo">
+		<div>
+			<h2>Uma vida com Deus</h2>
+			<p>
+				Ouça a palavra de Deus de uma maneira leve, diversos artigos para
+				meditar e refletir.
+			</p>
+			<article v-for="(artigo, index) in artigos" :key="index">
+				<div>
+					<img :src="tempoRefletir" alt="Minha Imagem" />
+					<img :src="artigo.imagem"	alt="Minha Imagem" class="img" />
+				</div>
+				<section>
+					<legend>
+						<date :datetime="artigo.data">{{artigo.data}}</date>
+						<a href="#">{{ artigo.categoria }}</a>
+					</legend>
+					<div class="conteudo">
+						<h3>{{ artigo.titulo }}</h3>
+						<p>{{ artigo.conteudo }}</p>
+					</div>
+					<div class="autor">
+						<img :src="amiltonMenezes" alt="amilton"/>
+						<cite>Pastor{{ artigo.autor }}</cite>
+					</div>
+				</section>
+			</article>
+		</div>
+	</div>
+</template>
+
 <script>
 import tempoRefletir from "@/assets/imagens/tempoRefletir.jpg";
 import amiltonMenezes from "@/assets/imagens/amiltonMenezes.jpg";
@@ -27,69 +59,51 @@ export default {
 	},
 };
 </script>
-<template>
-	<div class="w-full h-full mx-10 py-10 px-8 bg-white">
-		<div class="w-full">
-			<h2 class="text-4xl font-bold">
-				Uma vida com Deus
-			</h2>
-			<p class="mt-2 text-lg">
-				Ouça a palavra de Deus de uma maneira leve, diversos artigos para
-				meditar e refletir.
-			</p>
-			<div class="mt-20 space-y-20">
-				<article
-					v-for="(artigo, index) in artigos"
-					:key="index"
-					class="relative isolate gap-8 flex justify-between">
-					<div
-						class="relative">
-						<img :src="tempoRefletir" alt="Minha Imagem" />
-						<img
-							:src="artigo.imagem"
-							alt="Minha Imagem"
-							class="absolute inset-0 h-full w-full" />
-						
-					</div>
 
-					<div class="w-full mx-20 ">
-						<div class="flex items-center gap-4 text-xs">
-							<time :datetime="artigo.data">{{artigo.data}}</time>
-							<a href="#" class="relative z-10 rounded-full bg-gray-100 px-3 py-1.5 ">{{ artigo.categoria }}</a>
-						</div>
-						<div class="w-full">
-							<h3
-								class="mt-3 text-lg font-semibold">
-								<a href="#">
-									<span class="w-full"></span>
-									{{ artigo.titulo }}
-								</a>
-							</h3>
-							<p class="mt-5 text-sm">
-								{{ artigo.conteudo }}
-							</p>
-						</div>
-						<div class="mt-6 flex border-t pt-6">
-							<div class="relative flex items-center gap-4">
-								<img
-									:src="amiltonMenezes"
-									alt="amilton"
-									class="h-10 w-10 rounded-full" />
-								<div class="text-sm leading-6">
-									<p class="font-semibold">
-										<a href="#">
-											<span class="absolute inset-0"></span>
-											{{ artigo.autor }}
-										</a>
-									</p>
-									<p>Pastor</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</article>
-			</div>
-		</div>
-	</div>
-	
-</template>
+<style scoped postcss>
+#artigo{
+	@apply w-full h-full mx-10 py-10 px-8 bg-white;
+	h2 {
+		@apply text-4xl font-bold
+	};
+	p{
+		@apply mt-2 text-lg;
+	}
+	article{
+		@apply flex justify-between gap-8 mt-10 space-y-20;
+		div{
+			@apply relative;
+		.img{
+			@apply absolute inset-0 h-full w-full
+		}
+		}
+		section{
+			@apply w-full mx-10;
+			legend{
+				@apply flex items-center gap-4 text-xs;
+				a{
+					@apply relative z-10 rounded-full bg-gray-100 px-3 py-1.5;
+				}
+			}
+			.conteudo{
+				@apply w-full;
+				h3{
+					@apply mt-3 text-lg font-semibold;
+				}
+				p{
+					@apply mt-5 text-sm;
+				}
+			}
+			.autor{
+				@apply flex flex-col items-start mt-6 gap-1;
+				img{
+					@apply h-10 w-10 rounded-full;
+				}
+				cite{
+					@apply font-semibold;
+				}
+			}
+		}
+	}
+}
+</style>
