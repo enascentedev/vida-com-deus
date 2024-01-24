17dce075e3de6ea5995bd0cfab0e2aea7b92d7e5
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-01-23 22:31:22 -0300

    refactor: classes

--- front-end/src/components/header.vue
+++ front-end/src/components/header.vue
@@ -1,3 +1,28 @@
+<template>
+	<header>
+		<nav class="flex items-center justify-between px-10 py-7 bg-info opacity-40">
+			<router-link to="/livros">
+				<a href="#">
+					<img class="h-8 w-auto"
+						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi0ZRdV_U9tw_Iz7_eO2R74xNCqmZk3ObmNQ&usqp=CAU"
+						alt="logo" />
+				</a>
+			</router-link>
+				<router-link to="/livros">
+					<a href="#" class="text-sm font-semibold text-white">Leitura diária</a>
+				</router-link>
+				<router-link to="/artigo-semanal">
+					<a href="#" class="text-sm font-semibold text-white">Artigo semanal</a>
+				</router-link>
+				<router-link to="/home">
+					<a href="#" class="text-sm font-semibold text-white">Home</a>
+				</router-link>
+				<router-link to="/login">
+					<a href="#" class="text-sm font-semibold text-white" @click="logout()">Sair</a>
+				</router-link>
+		</nav>
+	</header>
+</template>
 <script>
 import { StoreUser } from "@/stores/user";
 export default {
@@ -18,111 +43,8 @@ export default {
 				this.storelogout.logout();
 			}
 			localStorage.removeItem("authToken");
-
-			// Redireciona o usuário de volta para a página de login
 			this.$router.push("/login");
 		},
 	},
 };
 </script>
-
-<template>
-	<header class="bg-info opacity-40">
-		<nav
-			class="mx-auto flex max-w-7xl items-center justify-around p-6 lg:px-8"
-			aria-label="Global">
-			<div class="flex lg:flex-1">
-				<a href="#" class="-m-1.5 p-1.5">
-					<span class="sr-only">Your Company</span>
-					<img
-						class="h-8 w-auto"
-						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi0ZRdV_U9tw_Iz7_eO2R74xNCqmZk3ObmNQ&usqp=CAU"
-						alt="" />
-				</a>
-			</div>
-			<div class="flex lg:hidden">
-				<button
-					type="button"
-					class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-indigo-300">
-					<span class="sr-only">Open main menu</span>
-					<svg
-						class="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						aria-hidden="true">
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-					</svg>
-				</button>
-			</div>
-
-			<div class="hidden lg:flex lg:gap-x-40">
-				<router-link to="/livros">
-					<a href="#" class="text-sm font-semibold leading-6 text-white"
-						>Leitura diária</a
-					>
-				</router-link>
-
-				<router-link to="/artigo-semanal">
-					<a href="#" class="text-sm font-semibold leading-6 text-white"
-						>Artigo semanal</a
-					></router-link
-				>
-				<router-link to="/home">
-					<a href="#" class="text-sm font-semibold leading-6 text-white"
-						>Home</a
-					></router-link
-				>
-			</div>
-			<div class="hidden lg:flex lg:flex-1 lg:justify-end"></div>
-			<div class="hidden lg:flex lg:flex-1 lg:justify-end">
-				<router-link to="/login">
-					<!-- v-show="this.storelogout.authToken" -->
-					<a
-						href="#"
-						class="text-sm font-semibold leading-6 text-white"
-						@click="logout()"
-						>Sair<span aria-hidden="true">&rarr;</span></a
-					>
-				</router-link>
-			</div>
-		</nav>
-		<!-- Mobile menu, show/hide based on menu open state. -->
-		<div class="lg:hidden" role="dialog" aria-modal="true">
-			<!-- Background backdrop, show/hide based on slide-over state. -->
-			<div class="fixed inset-0 z-10"></div>
-			<div
-				class="right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-				<div class="flex items-center justify-between">
-					<a href="#" class="-m-1.5 p-1.5">
-						<span class="sr-only">Vida livre</span>
-						<img
-							class="h-8 w-auto"
-							src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-							alt="" />
-					</a>
-				</div>
-				<div class="mt-6 flow-root">
-					<div class="-my-6 divide-y divide-gray-500/10">
-						<div class="space-y-2 py-6">
-							<a
-								href="#"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-								>Leitura diária</a
-							>
-							<a
-								href="#"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-								>Artigo semanal</a
-							>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</header>
-</template>
