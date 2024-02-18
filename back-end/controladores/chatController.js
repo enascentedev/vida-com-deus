73c573f1bad1275b5e7ea7fad66f43a3f1101e11
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-02-18 19:53:10 -0300

    feat: adicionando config api chat gpt e adicionando campos no json

--- back-end/controladores/chatController.js
+++ back-end/controladores/chatController.js
@@ -6,18 +6,22 @@ exports.enviarPergunta = async (req, res) => {
   const pergunta = req.body.pergunta;
 
   try {
-    const response = await fetch('https://api.openai.com/v1/completions', {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${OPENAI_API_KEY}`
       },
       body: JSON.stringify({
-        model: "text-davinci-003", // Especifica o modelo
-        prompt: pergunta, // pergunta
-        max_tokens: 100, // Limite de tokens na resposta
-				temperature: 0.5
-      })
+				"model": "gpt-3.5-turbo",
+				"messages": [
+					{
+						"role": "user",
+						"content": "Qual é a capital do estado do Rio Grande do Norte?"
+					}
+				],
+				"max_tokens": 50
+			})
     });
 
     if (!response.ok) {
